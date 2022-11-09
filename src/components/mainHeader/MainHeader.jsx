11a27e7ae741d4f4940e927b390b18fbb16fd765
forhadmia1 <forhad8613@gmail.com>
2022-11-09 18:38:41 +0600

    navbar responsive fixed

--- src/components/mainHeader/MainHeader.jsx
+++ src/components/mainHeader/MainHeader.jsx
@@ -6,13 +6,13 @@ import '../mainHeader/MainHeader.css'
 const MainHeader = () => {
     return (
         <nav id='navbar' className='d-flex px-4 py-2 align-items-center'>
-            <div className='d-flex justify-content-center align-items-center left-side w-full'>
+            <div className='d-flex justify-content-center align-items-center left-side w-full h-100'>
                 <Link to={'/'}>
-                    <img className='w-50' src={logo} alt="" />
+                    <img className='logo' src={logo} alt="" />
                 </Link>
             </div>
             <div className='right-side'>
-                <ul className='d-flex list-unstyled fs-5 m-0'>
+                <ul className='d-flex flex-column flex-md-row list-unstyled fs-5 m-0'>
                     <li><Link to={''} className='text-decoration-none link'>
                         8750270991, 9873580735
                     </Link></li>
@@ -22,7 +22,6 @@ const MainHeader = () => {
                     <li><Link to={''} className='text-decoration-none link'>
                         10:00am to 8:00pm
                     </Link></li>
-
                 </ul>
             </div>
         </nav>
