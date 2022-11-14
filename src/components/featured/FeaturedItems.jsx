d218f650c78f0c5e643d5ecd27ce752cc1897251
forhadmia1 <forhad8613@gmail.com>
2022-11-14 13:01:10 +0600

    contact section height fixed

--- src/components/featured/FeaturedItems.jsx
+++ src/components/featured/FeaturedItems.jsx
@@ -10,13 +10,19 @@ import bike6 from '../../assets/images/bikes/bull7.jpg'
 
 const FeaturedItems = () => {
     return (
-        <section id='gallery' className='bg-dark pb-5'>
-            <div className='pt-3 pb-2 featured-title'>
-                <h1 className='text-center text-white fw-bold'>Motorcycles</h1>
+        <section id='gallery'>
+            <div className='pt-5 featured-title '>
+                <p className='text-center fw-bold'>TAKING RIDES TO A NEWER LEVEL</p>
+                <h1 className='text-center fw-bold'>GREAT PERFORMANCE THAT
+                    <br /> MATTERS IN FUTURE</h1>
             </div>
             <Container className='mt-5 px-3'>
                 <Row xs={1} md={3} className='gap-4 gap-md-0 py-5'>
                     <Col>
+                        <div className='px-3'>
+                            <h6 className='text-center fw-bold'>CUTTING EDGE TECH</h6>
+                            <p className='text-center text-secondary'>Exercitation ullamco laboris nis exa duis aute irure dolor.</p>
+                        </div>
                         <Card className='card-item'>
                             <Card.Img variant="top" src={bike1} />
                             <div className='card-item-inner'>
@@ -25,6 +31,10 @@ const FeaturedItems = () => {
                         </Card>
                     </Col>
                     <Col>
+                        <div className='px-3'>
+                            <h6 className='text-center fw-bold'>PERFECT STYLING</h6>
+                            <p className='text-center text-secondary'>Exercitation ullamco laboris nis exa duis aute irure dolor.</p>
+                        </div>
                         <Card className='card-item'>
                             <Card.Img variant="top" src={bike3} />
                             <div className='card-item-inner'>
@@ -33,6 +43,10 @@ const FeaturedItems = () => {
                         </Card>
                     </Col>
                     <Col>
+                        <div className='px-3'>
+                            <h6 className='text-center fw-bold'>DISTINCTIVE BEAUTY</h6>
+                            <p className='text-center text-secondary'>Exercitation ullamco laboris nis exa duis aute irure dolor.</p>
+                        </div>
                         <Card className='card-item'>
                             <Card.Img variant="top" src={bike5} />
                             <div className='card-item-inner'>
